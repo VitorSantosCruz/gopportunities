fa56ebbe0eb4f11533ad1c3b01a79868afd95b91
Vítor Cruz <vittorcruz18101998@gmail.com>
2025-01-31 07:01:44 -0300

    feat: add getOpenings handler logic

--- handler/show_openings.go
+++ handler/show_openings.go
@@ -3,11 +3,18 @@ package handler
 import (
 	"net/http"
 
+	"github.com/VitorSantosCruz/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func GetOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		logger.Errorf("Error get-openings: %v", err)
+		sendError(ctx, http.StatusInternalServerError, "error get-openings")
+		return
+	}
+
+	sendSuccess(ctx, http.StatusOK, "get-openings", openings)
 }
